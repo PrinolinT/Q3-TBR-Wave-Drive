97af7daf73ac66f4c7b8c0e064da4179742a24be
PrinolinT <prinolin.thaver@dunlop.co.za>
2026-06-04 15:06:39 +0200

    Refactor Firebase initialization and config
    Updated Firebase configuration and imports.

--- firebase.js
+++ firebase.js
@@ -1,51 +1,17 @@
-// firebase.js
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-import {
-  initializeApp
-}
-from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
-
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  doc,
-  setDoc,
-  getDoc,
-  getDocs,
-  updateDoc,
-  serverTimestamp
-}
-from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
-
+// Your web app's Firebase configuration
 const firebaseConfig = {
-
-  apiKey: "AIzaSyDg0vFZwgUnb1Cjer7w-wb95_YjG8MDw54",
-
-  authDomain: "q3-suv-wave.firebaseapp.com",
-
-  projectId: "q3-suv-wave",
-
-  storageBucket: "q3-suv-wave.firebasestorage.app",
-
-  messagingSenderId: "785277358729",
-
-  appId: "1:785277358729:web:e326909768b69606452c58"
-
+  apiKey: "AIzaSyBEr60ly1rGZThmFI9DamgW8xzgxJ6wwl8",
+  authDomain: "q3-tbr-wave---1.firebaseapp.com",
+  projectId: "q3-tbr-wave---1",
+  storageBucket: "q3-tbr-wave---1.firebasestorage.app",
+  messagingSenderId: "436232639814",
+  appId: "1:436232639814:web:f7a3cfbb423701c6318147"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-const db = getFirestore(app);
-
-export {
-  db,
-  collection,
-  addDoc,
-  doc,
-  setDoc,
-  getDoc,
-  getDocs,
-  updateDoc,
-  serverTimestamp
-};
